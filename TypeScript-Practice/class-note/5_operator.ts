e2f68aba6cc780a930c1c44750244d7b92e78d86
dnjstd <dnjstd@gmail.com>
2022-03-01 21:56:50 +0900

    인터섹션 타입

--- TypeScript-Practice/class-note/5_operator.ts
+++ TypeScript-Practice/class-note/5_operator.ts
@@ -15,6 +15,7 @@ logMessage("hello");
 logMessage(100);
 
 //유니온 타입의 특징
+
 interface Developer {
   name: string;
   skill: string;
@@ -25,6 +26,20 @@ interface Person {
   age: number;
 }
 
-function askSomeone(someone: Developer | Person) {
+// function askSomeone(someone: Developer | Person) {
+//   someone.name; //공통된 속성만 사용 가능
+// }
+// askSomeone({ name: "soyeon", skill: "웹 개발" });
+// askSomeone({ name: "soyeon", age: 27 });
+
+//인터섹션 타입
+function askSomeone(someone: Developer & Person) {
   someone.name;
+  someone.skill;
+  someone.age;
 }
+
+askSomeone({ name: "soyeon", skill: "웹 개발", age: 27 });
+
+// var seho: string | number | boolean;
+// var seho: string & number & boolean;
