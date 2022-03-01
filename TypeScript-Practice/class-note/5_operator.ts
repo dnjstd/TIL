7e8a8a706df22d6ce94a2c23e4b97a1a9c21c064
dnjstd <dnjstd@gmail.com>
2022-03-01 21:43:25 +0900

    유니온 타입

--- TypeScript-Practice/class-note/5_operator.ts
+++ TypeScript-Practice/class-note/5_operator.ts
@@ -1,8 +1,30 @@
 //union type: 특정 변수나 파라미터에 한가지 이상의 타입을 쓰고 싶을때 사용
 
+var seho: string | number | boolean;
 function logMessage(value: string | number) {
-  console.log(value);
+  if (typeof value === "number") {
+    value.toLocaleString();
+  }
+  if (typeof value === "string") {
+    value.toString();
+  }
+  throw new TypeError("value must be string or number");
 }
 
 logMessage("hello");
 logMessage(100);
+
+//유니온 타입의 특징
+interface Developer {
+  name: string;
+  skill: string;
+}
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+function askSomeone(someone: Developer | Person) {
+  someone.name;
+}
