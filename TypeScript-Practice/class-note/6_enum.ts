3012aa51cd587690c20de6b1de07f6aa420a79a0
dnjstd <dnjstd@gmail.com>
2022-03-02 22:42:59 +0900

    enum

--- TypeScript-Practice/class-note/6_enum.ts
+++ TypeScript-Practice/class-note/6_enum.ts
@@ -13,3 +13,21 @@ enum Shoes {
 
 var myShoes = Shoes.Adidas;
 console.log(myShoes); //숫자형 - 0 //문자형 - '아디다스'
+
+//이넘 예제
+enum Answer {
+  Yes = "Y",
+  No = "N",
+}
+
+function askQuestion(answer: Answer) {
+  if (answer === Answer.Yes) {
+    console.log("정답입니다");
+  }
+  if (answer === Answer.No) {
+    console.log("오답입니다");
+  }
+}
+
+askQuestion(Answer.Yes);
+askQuestion(Answer.No);
